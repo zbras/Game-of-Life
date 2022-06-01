2ffe3b67b6a9d512a59224ac50d39b538a78818a
Zachary Brasili <90160491+zbras@users.noreply.github.com>
2022-05-31 21:00:46 -0700

    Update game.py

--- game.py
+++ game.py
@@ -3,99 +3,105 @@ from states.title_menu import TitleMenu
 
 class Game():
 
-	def __init__(self, screen_width, screen_height):
+    def __init__(self, screen_width, screen_height):
 
-		pygame.init()
-		pygame.mouse.set_visible(False)
+        pygame.init()
+        pygame.mouse.set_visible(False)
 
-		self.running, self.playing = True, True
-		self.delta_time, self.prev_time = 0, 0
-		self.screen_width, self.screen_height = screen_width, screen_height
-		self.canvas = pygame.Surface((screen_width, screen_height))
-		self.screen = pygame.display.set_mode((screen_width, screen_height), pygame.NOFRAME)
-		self.actions = {
-			'left': False,
-			'right': False,
-			'up': False,
-			'down': False,
-			'tab': False,
-			'escape': False,
-			'start': False
-		}
-		self.state_stack = []
+        self.running, self.playing = True, True
+        self.delta_time, self.prev_time = 0, 0
+        self.screen_width, self.screen_height = screen_width, screen_height
+        self.canvas = pygame.Surface((screen_width, screen_height))
+        self.screen = pygame.display.set_mode((screen_width, screen_height), pygame.NOFRAME)
+        self.actions = {
+            'left': False,
+            'right': False,
+            'up': False,
+            'down': False,
+            'tab': False,
+            'escape': False,
+            'start': False
+        }
+        self.state_stack = []
 
-		self.load_assets()
-		self.load_states()
+        self.load_assets()
+        self.load_states()
 
-	def game_loop(self):
+    def game_loop(self):
 
-		while self.playing:
-			self.get_delta_time()
-			self.get_events()
-			self.update()
-			self.render()
+        while self.playing:
+            self.get_delta_time()
+            self.get_events()
+            self.update()
+            self.render()
 
-			if self.actions['escape']:
-				self.running, self.playing = False, False
-				quit()
+            if self.actions['escape']:
+                self.running, self.playing = False, False
+                quit()
 
-	def get_events(self):
+    def get_events(self):
 
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				self.running, self.playing = False, False
-				quit()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running, self.playing = False, False
+                quit()
 
-			if event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_TAB:
-					self.actions['tab'] = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_TAB:
+                    self.actions['tab'] = True
 
-				if event.key == pygame.K_ESCAPE:
-					self.actions['escape'] = True
+                if event.key == pygame.K_ESCAPE:
+                    self.actions['escape'] = True
 
-				if event.key == pygame.K_RETURN:
-					self.actions['start'] = True
+                if event.key == pygame.K_RETURN:
+                    self.actions['start'] = True
 
-			if event.type == pygame.KEYUP:
-				if event.key == pygame.K_TAB:
-					self.actions['tab'] = False
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_TAB:
+                    self.actions['tab'] = False
 
-				if event.key == pygame.K_ESCAPE:
-					self.actions['escape'] = False
+                if event.key == pygame.K_ESCAPE:
+                    self.actions['escape'] = False
 
-				if event.key == pygame.K_RETURN:
-					self.actions['start'] = False
+                if event.key == pygame.K_RETURN:
+                    self.actions['start'] = False
 
-	def update(self):
-		self.state_stack[-1].update(self.delta_time, self.actions)
+    def update(self):
 
-	def render(self):
-		self.state_stack[-1].render(self.canvas)
-		pygame.display.flip()
+        self.state_stack[-1].update(self.delta_time, self.actions)
 
-	def get_delta_time(self):
-		now = time.time()
-		self.delta_time = now - self.prev_time
-		self.prev_time = now
+    def render(self):
 
-	def draw_text(self, surface, text, color, x, y):
-		text_surface = self.font.render(text, True, color)
-		text_rect = text_surface.get_rect()
-		text_rect.center = (x, y)
-		surface.blit(text_surface, text_rect)
+        self.state_stack[-1].render(self.canvas)
+        pygame.display.flip()
 
-	def load_assets(self):
-		self.assets_dir = os.path.join('assets')
-		self.font_dir = os.path.join(self.assets_dir, 'fonts')
-		self.font = pygame.font.Font(os.path.join(self.font_dir, 'PressStart2P-Regular.ttf'), 30)
+    def get_delta_time(self):
 
-	def load_states(self):
-		self.title_screen = TitleMenu(self)
-		self.state_stack.append(self.title_screen)
+        now = time.time()
+        self.delta_time = now - self.prev_time
+        self.prev_time = now
 
-	def reset_keys(self):
-		for action in self.actions:
-			self.actions[action] = False
+    def draw_text(self, surface, text, color, x, y):
+
+        text_surface = self.font.render(text, True, color)
+        text_rect = text_surface.get_rect()
+        text_rect.center = (x, y)
+        surface.blit(text_surface, text_rect)
+
+    def load_assets(self):
+
+        self.assets_dir = os.path.join('assets')
+        self.font_dir = os.path.join(self.assets_dir, 'fonts')
+        self.font = pygame.font.Font(os.path.join(self.font_dir, 'PressStart2P-Regular.ttf'), 30)
+
+    def load_states(self):
+
+        self.title_screen = TitleMenu(self)
+        self.state_stack.append(self.title_screen)
+
+    def reset_keys(self):
+        for action in self.actions:
+            self.actions[action] = False
 
 if __name__ == '__main__':
     quit()
