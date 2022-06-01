f7a4e908eff0b28d1b89aabea604d7ae7f826895
Zachary Brasili <90160491+zbras@users.noreply.github.com>
2022-05-31 21:00:40 -0700

    Update title_menu.py

--- states/title_menu.py
+++ states/title_menu.py
@@ -3,22 +3,22 @@ from states.board import Board
 
 class TitleMenu(State):
 
-	def __init__(self, game):
+    def __init__(self, game):
 
-		State.__init__(self, game)
+        State.__init__(self, game)
 
-	def update(self, delta_time, actions):
+    def update(self, delta_time, actions):
 
-		if actions['start']:
-			new_state = Board(self.game)
-			new_state.enter_state()
+        if actions['start']:
+            new_state = Board(self.game)
+            new_state.enter_state()
 
-		self.game.reset_keys()
+        self.game.reset_keys()
 
-	def render(self, display):
+    def render(self, display):
 
-		self.game.screen.blit(self.game.canvas, (0, 0))
-		self.game.draw_text(display, 'Game of Life', (255, 255, 255), self.game.screen_width / 2, self.game.screen_height / 2)
+        self.game.screen.blit(self.game.canvas, (0, 0))
+        self.game.draw_text(display, 'Game of Life', (255, 255, 255), self.game.screen_width / 2, self.game.screen_height / 2)
 
 if __name__ == '__main__':
     quit()
