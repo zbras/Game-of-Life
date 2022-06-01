bf59fc39610204abf573b26e77e49622d1408dc4
Zachary Brasili <90160491+zbras@users.noreply.github.com>
2022-05-31 21:00:42 -0700

    Update pause_menu.py

--- states/pause_menu.py
+++ states/pause_menu.py
@@ -3,19 +3,19 @@ from states.state import State
 
 class PauseMenu(State):
 
-	def __init__(self, game):
+    def __init__(self, game):
 
-		State.__init__(self, game)
+        State.__init__(self, game)
 
-		self.game = game
+        self.game = game
 
-	def update(self, delta_time, actions):
+    def update(self, delta_time, actions):
 
-		if actions['tab']:
-			self.exit_state()
+        if actions['tab']:
+            self.exit_state()
 
-		self.game.reset_keys()
+        self.game.reset_keys()
 
-	def render(self, display):
+    def render(self, display):
 
-		self.prev_state.render(display)
+        self.prev_state.render(display)
